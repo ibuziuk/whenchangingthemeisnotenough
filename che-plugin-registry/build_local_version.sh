2f84cd8becd88212b9de5d4e50d355823a4d57bd
Sergii Leshchenko <sleshche@redhat.com>
2018-10-19 23:18:39 +0300

    Use  tag instead of latest

--- che-plugin-registry/build_local_version.sh
+++ che-plugin-registry/build_local_version.sh
@@ -1,3 +1,5 @@
-docker build -t eclipse/che-plugin-registry .
+docker build -t eclipse/che-plugin-registry:local .
 
-export PLUGIN_REGISTRY_IMAGE_PULL_POLICY=IfNotPresent
+export PLUGIN_REGISTRY_IMAGE_TAG=local
+export PLUGIN_REGISTRY_IMAGE="eclipse/che-plugin-registry"
+export PLUGIN_REGISTRY_IMAGE_PULL_POLICY="IfNotPresent"
