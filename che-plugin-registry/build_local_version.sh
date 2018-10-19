c9c82f98aba45d9b368fdf9367c634f9b2064e0b
Sergii Leshchenko <sleshche@redhat.com>
2018-10-19 23:45:51 +0300

    Improve build_local_version.sh

--- che-plugin-registry/build_local_version.sh
+++ che-plugin-registry/build_local_version.sh
@@ -1,5 +1,5 @@
-docker build -t eclipse/che-plugin-registry:local .
-
 export PLUGIN_REGISTRY_IMAGE_TAG=local
 export PLUGIN_REGISTRY_IMAGE="eclipse/che-plugin-registry"
 export PLUGIN_REGISTRY_IMAGE_PULL_POLICY="IfNotPresent"
+
+docker build -t ${PLUGIN_REGISTRY_IMAGE}:${PLUGIN_REGISTRY_IMAGE_TAG} .
