3b288f796f5d9fdc7e46a608ac141f47ab6f1763
Sergii Leshchenko <sleshche@redhat.com>
2018-10-23 18:28:05 +0300

    add setting 172.17.0.1 for OCP

--- run-ocp-with-che.sh
+++ run-ocp-with-che.sh
@@ -1,3 +1,6 @@
+export OC_PUBLIC_HOSTNAME=172.17.0.1
+export OC_PUBLIC_IP=172.17.0.1
+
 export PLUGIN_REGISTRY_IMAGE_TAG=latest
 export PLUGIN_REGISTRY_IMAGE="demo/che-plugin-registry"
 export PLUGIN_REGISTRY_IMAGE_PULL_POLICY="IfNotPresent"
